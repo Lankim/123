c92631febac5205771c395ba22f4cb0b1e39ba13
Lankim <46came@gmail.com>
2024-10-07 16:08:40 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// script.js 파일
+function playBritish() {
+    var audio = document.getElementById("british-audio");
+    audio.play();
+}
+
+function playAmerican() {
+    var audio = document.getElementById("american-audio");
+    audio.play();
+}
